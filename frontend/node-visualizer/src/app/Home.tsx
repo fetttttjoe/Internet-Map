cbb84f00b61f6af57ed3e12f4d156bccb065a557
fetttttjoe <fetttttjoe@gmail.com>
2023-12-09 22:32:21 +0100

    rebase

--- frontend/node-visualizer/src/app/Home.tsx
+++ frontend/node-visualizer/src/app/Home.tsx
@@ -5,9 +5,9 @@ import TreeGraph from './components/TreeGraph'
 
 const Home: React.FC = () => {
   return (
-    <div className={`flex items-center justify-center bg-[#c4cad3] p-4`}>
-      <div className={` h-full w-full bg-[#23395B] shadow-lg`}>
-        <h1 className={`m-2 mb-6 text-4xl font-bold text-white`}>
+    <div className={`flex items-center justify-center bg-[#c4cad3]`}>
+      <div className={`h-full w-full bg-[#23395B] shadow-lg`}>
+        <h1 className={`rounded m-4 text-4xl font-bold text-white`}>
           Tree Structure
         </h1>
         <TreeGraph />
